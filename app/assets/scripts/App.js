724b520c7838ed7dc2f287607bd55337dc84a7af
Eduard-Benedic <benediceduard@outlook.com>
2019-02-03 11:57:24 +0000

    completed desktop and mobileheader style

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -14,6 +14,7 @@ class MobileMenu {
      toggleTheMenu() {
         this.menuContent.classList.toggle('site-header__menu-content--visible');
         this.siteHeader.classList.toggle('site-header--expanded');
+        this.menuIcon.classList.toggle('site-header__menu-icon--close-x');
     }
 }
 
