62980b0bacf5746e2739d2a4565ceb3ac7cf9fd3
Eduard-Benedic <benediceduard@outlook.com>
2019-02-03 18:20:06 +0000

    awesome animation completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,5 @@
+/*--- Code used for the header----- */
+
 class MobileMenu {
     constructor() {
         this.siteHeader = document.querySelector('.site-header');
@@ -32,3 +34,75 @@ class MobileMenu {
 
 
 var mobileMenu = new MobileMenu();
+
+
+/* Code used for reaveal-on-scroll */
+
+
+class RevealOnScroll {
+    constructor() {
+        this.itemsToReveal = document.querySelectorAll('.feature-item');
+        this.scrollToShow();
+       
+    }
+
+    scrollToShow() {
+        window.addEventListener('scroll', this.hideInitially.bind(this), false);
+    }
+    
+    hideInitially() {
+        var windowYHeight = window.innerHeight;
+        /*var item = this.itemsToReveal.getBoundingClientRect().bottom;
+        console.log(windowYHeight);
+        console.log(item);
+       if (item < windowYHeight*0.9) {
+           this.itemsToReveal.classList.add('reveal-item');
+       }*/
+       this.itemsToReveal.forEach(function(items){
+            var itemsCoordinates = items.getBoundingClientRect().bottom;
+            if (itemsCoordinates < windowYHeight) {
+                items.classList.add('reveal-item');
+            } else {
+                items.classList.remove('reveal-item');
+            }
+       });
+      
+    }
+}
+
+var revealOnScroll = new RevealOnScroll();
+
+
+
+class RevealOnScrollTestiomonial {
+    constructor() {
+        this.itemsToReveal = document.querySelectorAll('.testimonial');
+        this.scrollToShow();
+       
+    }
+
+    scrollToShow() {
+        window.addEventListener('scroll', this.hideInitially.bind(this), false);
+    }
+    
+    hideInitially() {
+        var windowYHeight = window.innerHeight;
+        /*var item = this.itemsToReveal.getBoundingClientRect().bottom;
+        console.log(windowYHeight);
+        console.log(item);
+       if (item < windowYHeight*0.9) {
+           this.itemsToReveal.classList.add('reveal-item');
+       }*/
+       this.itemsToReveal.forEach(function(items){
+            var itemsCoordinates = items.getBoundingClientRect().bottom;
+            if (itemsCoordinates < windowYHeight) {
+                items.classList.add('testimonial-reveal');
+            } else {
+                items.classList.remove('testimonial-reveal');
+            }
+       });
+      
+    }
+}
+
+var testimonialReveal = new RevealOnScrollTestiomonial();
