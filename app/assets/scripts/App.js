bd0d5cf364e7184f0e84065633b08f37c2f83702
Eduard-Benedic <benediceduard@outlook.com>
2019-02-05 11:18:04 +0000

    site completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -131,3 +131,27 @@ class StickyHeader {
     }
 }
 var stickyHeader = new StickyHeader();
+
+
+// Get the modal element
+
+var modal = document.querySelector('.modal');
+
+// Get the open modal button
+
+var button = document.querySelector('.btn');
+var closeBtn = document.querySelector('.btn--close');
+
+button.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+
+
+function openModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal () {
+    modal.style.display = 'none';
+}
+
+
